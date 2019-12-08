510e7a98be640155dc0509a8adc121aa0d685cb3
Alex <ateilhet@gmail.com>
2019-12-09 00:02:30 +0100

    day 5

--- day5.js
+++ day5.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 
-const part1 = () =>
+const part2 = () =>
   fs.readFile("./day5_input", "utf8", function(err, contents) {
-    let program = contents.split(",");
+    let program = contents.split(",").map(v => parseInt(v, 10));
     const l = program.length;
     let i = 0;
     let r = [];
     const getValue = (idx, paramNb, mode) => {
       if (mode === 0) {
-        const address = parseInt(program[idx + paramNb], 10);
-        return parseInt(program[address], 10);
+        const address = program[idx + paramNb];
+        return program[address];
       } else {
-        return parseInt(program[idx + paramNb], 10);
+        return program[idx + paramNb];
       }
     };
     while (i < l) {
@@ -26,10 +26,11 @@ const part1 = () =>
             .split("")
             .map(m => parseInt(m, 10))
             .reverse();
+          modes.length < 2 && modes.push(0);
           const modeValues = modes.map((mode, paramNb) =>
             getValue(i, paramNb + 1, mode)
           );
-          modeValues.push(parseInt(program[i + 3], 10));
+          modeValues.push(program[i + 3]);
 
           program[modeValues[2]] =
             op === 1
@@ -37,8 +38,20 @@ const part1 = () =>
               : modeValues[0] * modeValues[1];
           i = i + 4;
         } else if (op === 4) {
-          r.push(program[parseInt(program[i + 1], 10)]);
+          r.push(program[i + 1]);
           i = i + 2;
+        } else if (op === 5) {
+          program[i + 1] !== 0 ? (i = program[i + 2]) : (i = i + 3);
+        } else if (op === 6) {
+          program[i + 1] === 0 ? (i = program[i + 2]) : (i = i + 3);
+        } else if (op === 7) {
+          program[i + 1] < program[i + 2]
+            ? (program[program[i + 3]] = 1)
+            : (program[program[i + 3]] = 0);
+        } else if (op === 8) {
+          program[i + 1] === program[i + 2]
+            ? (program[program[i + 3]] = 1)
+            : (program[program[i + 3]] = 0);
         } else {
           console.error("wtf", instruction);
           break;
@@ -46,20 +59,32 @@ const part1 = () =>
       } else {
         const op = parseInt(instruction, 10);
         if (op === 1 || op === 2) {
-          const address1 = parseInt(program[i + 1], 10);
-          const address2 = parseInt(program[i + 2], 10);
-          const pos = parseInt(program[i + 3], 10);
+          const address1 = program[i + 1];
+          const address2 = program[i + 2];
+          const pos = program[i + 3];
           program[pos] =
             op === 1
-              ? parseInt(program[address1], 10) + parseInt(program[address2], 10)
-              : parseInt(program[address1], 10) * parseInt(program[address2], 10);
+              ? program[address1] + program[address2]
+              : program[address1] * program[address2];
           i = i + 4;
         } else if (op === 3) {
-          program[parseInt(program[i + 1], 10)] = 1;
+          program[program[i + 1]] = 8;
           i = i + 2;
         } else if (op === 4) {
-          r.push(program[parseInt(program[i + 1], 10)]);
+          r.push(program[program[i + 1]]);
           i = i + 2;
+        } else if (op === 5) {
+          program[i + 1] !== 0 ? (i = program[i + 2]) : (i = i + 3);
+        } else if (op === 6) {
+          program[i + 1] === 0 ? (i = program[i + 2]) : (i = i + 3);
+        } else if (op === 7) {
+          program[i + 1] < program[i + 2]
+            ? (program[program[i + 3]] = 1)
+            : (program[program[i + 3]] = 0);
+        } else if (op === 8) {
+          program[i + 1] === program[i + 2]
+            ? (program[program[i + 3]] = 1)
+            : (program[program[i + 3]] = 0);
         } else {
           console.error("wtf2", instruction);
           break;
@@ -70,4 +95,4 @@ const part1 = () =>
     console.log(r);
   });
 
-part1();
+part2();
