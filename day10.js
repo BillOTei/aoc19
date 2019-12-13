5999ad38f8e6aba0ce39a2e68700dd3220cfd8d3
Alex <ateilhet@gmail.com>
2019-12-13 22:41:00 +0100

    day 10 part 2 needs sort of asteroids map

--- day10.js
+++ day10.js
@@ -33,6 +33,24 @@ fs.readFile("./day10_input", "utf8", function(err, contents) {
       )
     ].length;
   };
+  const inRange = ({ asteroid, asteroids }) => {
+    const jsonAsteroid = JSON.stringify({ x: 0, y: 0, p: "#" });
+    const relativeAsteroids = asteroids
+      .map(a => {
+        return { ...a, x: a.x - asteroid.x, y: a.y - asteroid.y };
+      })
+      .filter(ast => jsonAsteroid !== JSON.stringify(ast));
+    const asteroidsMap = new Map();
+    relativeAsteroids.forEach(a => {
+      const angle = Math.round(Math.atan2(a.y, a.x) * 1000000) / 1000000;
+      const angleKey = angle.toString();
+      if (asteroidsMap.has(angleKey))
+        asteroidsMap.set(angleKey, [...asteroidsMap.get(angleKey), a]);
+      else asteroidsMap.set(angleKey, [a]);
+    });
+
+    return { asteroid, asteroidsMap, length: asteroidsMap.size };
+  };
   const distributedAsteroids = input.reduce(
     (acc, a) => [
       ...acc,
@@ -43,6 +61,28 @@ fs.readFile("./day10_input", "utf8", function(err, contents) {
     ],
     []
   );
+  const distributedMap = distributedAsteroids.map(o => inRange(o));
+  const { asteroid, asteroidsMap, length } = distributedMap.reduce(function(
+    prev,
+    current
+  ) {
+    return prev.length > current.length ? prev : current;
+  });
 
-  console.log(Math.max(...distributedAsteroids.map(o => visible(o))));
+  let shoot = true;
+  let i = 0;
+  let asteroidShot;
+  while (shoot) {
+    asteroidsMap.forEach(l => {
+      if (l.length > 0) {
+        asteroidShot = l.splice(0, 1);
+        i++;
+        if (i === 200) {
+          shoot = false;
+        }
+      }
+    });
+  }
+
+  console.log((asteroidShot[0].x + asteroid.x) * 100 + (asteroidShot[0].y + asteroid.y));
 });
